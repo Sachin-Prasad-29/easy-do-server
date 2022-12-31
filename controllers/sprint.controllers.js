bcbb0d80e13b224fe0ac96a617b8118bdf8a52a3
Sachin kumar prasad <skpkorba9009@gmail.com>
2022-12-31 16:36:05 +0530

    Added sprint controller and service methods

--- controllers/sprint.controllers.js
+++ controllers/sprint.controllers.js
@@ -1,38 +1,75 @@
 const { createHttpError } = require('../errors/custom.error')
 
 const {
-  createSprintSvc,
-  getAllSprintSvc,
-  getSprintByIdSvc,
-  editSprintByIdSvc,
-  deleteSprintByIdSvc
+    createSprintSvc,
+    getAllSprintSvc,
+    getSprintByIdSvc,
+    editSprintByIdSvc,
+    deleteSprintByIdSvc,
 } = require('../services/sprint.services')
 
 const createSprint = async (req, res, next) => {
-  const response = await createSprintSvc()
-  res.status(201).json({ success: true, sprint: response })
+    const sprintReqData = req.body
+    console.dir(sprintReqData)
+    if (Object.keys(sprintReqData).length === 0) {
+        const httpError = createHttpError('Body is missing', 400)
+        next(httpError)
+        return
+    }
+    try {
+        const sprintData = await createSprintSvc(sprintReqData)
+        res.status(201).json({ success: true, sprint: sprintData })
+    } catch (error) {
+        const httpError = createHttpError(error.message, 400)
+        next(httpError)
+    }
 }
 const getAllSprint = async (req, res, next) => {
-  const response = await getAllSprintSvc()
-  res.status(201).json({ success: true, sprint: response })
+    const projectId = req.params.projectId
+    try {
+        const allSprint = await getAllSprintSvc(projectId)
+        res.status(201).json({ success: true, sprint: allSprint })
+    } catch (error) {
+        const httpError = createHttpError(error.message, 400)
+        next(httpError)
+    }
 }
 const getSprintById = async (req, res, next) => {
-  const response = await getSprintByIdSvc()
-  res.status(201).json({ success: true, sprint: response })
+    const sprintId = req.params.sprintId
+    try {
+        const fetchedSprint = await getSprintByIdSvc(sprintId)
+        res.status(201).json({ success: true, sprint: fetchedSprint })
+    } catch (error) {
+        const httpError = createHttpError(error.message, 400)
+        next(httpError)
+    }
 }
 const editSprintById = async (req, res, next) => {
-  const response = await editSprintByIdSvc()
-  res.status(201).json({ success: true, sprint: response })
+    const sprintId = req.params.sprintId
+    const sprintPayload = req.body
+    try {
+        const updatedSprint = await editSprintByIdSvc(sprintId, sprintPayload)
+        res.status(201).json({ success: true, sprint: updatedSprint })
+    } catch (error) {
+        const httpError = createHttpError(error.message, 400)
+        next(httpError)
+    }
 }
 const deleteSprintById = async (req, res, next) => {
-  const response = await deleteSprintByIdSvc()
-  res.status(201).json({ success: true, sprint: response })
+    const sprintId = req.params.sprintId
+    try {
+        const deletedSprint = await deleteSprintByIdSvc(sprintId)
+        res.status(201).json({ success: true, sprint: deletedSprint })
+    } catch (error) {
+        const httpError = createHttpError(error.message, 400)
+        next(httpError)
+    }
 }
 
 module.exports = {
-  createSprint,
-  getAllSprint,
-  getSprintById,
-  editSprintById,
-  deleteSprintById
+    createSprint,
+    getAllSprint,
+    getSprintById,
+    editSprintById,
+    deleteSprintById,
 }
--- services/sprint.services.js
+++ services/sprint.services.js
@@ -1,20 +1,110 @@
 const { createHttpError } = require('../errors/custom.error')
 const Sprint = require('../models/Sprint')
 
-const createSprintSvc = async () => {
-  return 'Hi From createSprintSvc'
+const createSprintSvc = async (sprintData) => {
+  try {
+      const insertedSprint = await Sprint.create(sprintData)
+      if (!insertedSprint) {
+          const error = createHttpError('Bad Request', 400)
+          throw error
+      }
+      return insertedSprint
+  } catch (error) {
+      if ((error.name = 'ValidationError')) {
+          const dbError = new Error(`Validation error : ${error.message}`)
+          dbError.type = 'ValidationError'
+          throw dbError
+      }
+      if (error.name === 'CastError') {
+          const dbError = new Error(`Data Type Error : ${error.message}`)
+          dbError.type = 'CastError'
+          throw dbError
+      }
+      throw error
+  }
 }
-const getAllSprintSvc = async () => {
-  return 'Hi From getAllSprintSvc'
+const getAllSprintSvc = async (projectId) => {
+  try {
+      const allSprint = await Sprint.find({ projectId })
+      if (!allSprint) {
+          const error = createHttpError('No Sprint Found for the Project', 400)
+          throw error
+      }
+      return allSprint
+  } catch (error) {
+      if (error.name === 'CastError') {
+          const dbError = new Error(`Data Type error : ${error.message}`)
+          dbError.type = 'CastError'
+          throw dbError
+      }
+      if (error.type === 'NotFound') {
+          throw error
+      }
+      throw error
+  }
 }
-const getSprintByIdSvc = async () => {
-  return 'Hi From getSprintByIdSvc'
+const getSprintByIdSvc = async (sprintId) => {
+ try {
+     const sprintDetails = await Sprint.findById(sprintId)
+     if (!sprintDetails) {
+         const error = createHttpError('No Sprint Found with Given Id', 404)
+         throw error
+     }
+     return sprintDetails
+ } catch (error) {
+     if (error.name === 'CastError') {
+         const dbError = new Error(`Data Type error : ${error.message}`)
+         dbError.type = 'CastError'
+         throw dbError
+     }
+     if (error.type === 'NotFound') {
+         throw error
+     }
+     throw error
+ }
 }
-const editSprintByIdSvc = async () => {
-  return 'Hi From editSprintByIdSvc'
+const editSprintByIdSvc = async (sprintId,sprintPayload) => {
+   try {
+       const updatedSprint = await Sprint.findByIdAndUpdate(sprintId, sprintPayload, {
+           new: true,
+           runValidators: true,
+       })
+       if (!updatedSprint) {
+           const error = createHttpError('No Sprint Found with Given Id', 404)
+           throw error
+       }
+       return updatedSprint
+   } catch (error) {
+       if (error.name === 'CastError') {
+           const dbError = new Error(`Data Type error : ${error.message}`)
+           dbError.type = 'CastError'
+           throw dbError
+       }
+       if (error.type === 'NotFound') {
+           throw error
+       }
+       throw error
+   }
 }
-const deleteSprintByIdSvc = async () => {
-  return 'Hi From deleteSprintByIdSvc'
+const deleteSprintByIdSvc = async (sprintId) => {
+  try {
+      const deletedSprint = await Sprint.findByIdAndDelete(sprintId)
+      if (!deletedSprint) {
+          const error = createHttpError('No Sprint Found with Given Id', 404)
+          throw error
+      }
+      return deletedSprint
+  } catch (error) {
+      if (error.name === 'CastError') {
+          const dbError = new Error(`Data Type error : ${error.message}`)
+          dbError.type = 'CastError'
+          throw dbError
+      }
+      if (error.type === 'NotFound') {
+          throw error
+      }
+      throw error
+  }
 }
 
 module.exports = {
